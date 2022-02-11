710cfc9a1961b6dac9ee3f63c1c14bc1a0f67c9b
Noah Welter <noahwelter@gmail.com>
2022-02-11 09:40:02 -0600

    Complete Easy Challenges - Point Mutations

---
+++ exercises/easy_challenges/dna.js
@@ -0,0 +1,18 @@
+class DNA {
+  constructor(sequence) {
+    this.sequence = sequence;
+  }
+
+  hammingDistance(sequence) {
+    let differences = 0;
+
+    for (let index in this.sequence) {
+      if (!this.sequence[index] || !sequence[index]) break;
+      if (this.sequence[index] !== sequence[index]) differences += 1;
+    }
+
+    return differences;
+  }
+}
+
+module.exports = DNA;
